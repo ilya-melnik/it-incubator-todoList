9b8415a49838c3e124aac96af9a7c77b940c93aa
ilya-melnik <ilay.melnik.kr@gmail.com>
2020-12-16 15:19:04 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+function App() {
+
+    const tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks} />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/ap.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = {
+    name: 'nina',
+    age: 23,
+}
+
+
+const {name} = person
+
+
