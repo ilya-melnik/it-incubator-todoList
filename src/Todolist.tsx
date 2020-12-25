e1b0f8c38d6b7a01754c65d91cf994b2512cfb89
ilya-melnik <ilay.melnik.kr@gmail.com>
2020-12-25 14:22:27 +0200

    2 start

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -19,6 +19,7 @@ export function Todolist(props: PropsType) {
             <button>+</button>
         </div>
         <ul>
+
             <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
             <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
             <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
