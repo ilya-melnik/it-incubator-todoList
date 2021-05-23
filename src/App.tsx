afbf84f7eb366975dadd00133ce6a56abd43d25d
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-05-23 18:18:54 +0300

    les 1.1 and

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
+
 import './App.css';
+import {Todolist} from "./Todolist";
+
+
 
 function App() {
+    const task1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+    const task2 = [
+        {id: 1, title: "Hello", isDone: true},
+        {id: 2, title: "I'm a professor", isDone: true},
+        {id: 3, title: "how are you", isDone: false}
+    ]
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={'What to learn'} tasks={task1}/>
+            <Todolist title={'Songs'}          tasks={task2}/>
+
         </div>
     );
 }
